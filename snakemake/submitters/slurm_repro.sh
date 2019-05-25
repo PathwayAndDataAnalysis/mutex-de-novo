a7c3e18e4a1066ed6375fa185c32d6005d4fce49
hannah413 <manningh@ohsu.edu>
2019-05-24 17:16:22 -0700

    Removed abspaths and addded nosched option

---
+++ snakemake/submitters/slurm_repro.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+# capture snakemake/ dir location in a variable
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#SNAKEDIR="$(readlink -f $DIR/../)/"
+
+echo SLURM_SUBMIT_DIR is $SLURM_SUBMIT_DIR
+SNAKEDIR=${SLURM_SUBMIT_DIR}/../
+
+# make log directory if it doesn't already exist
+mkdir -p ${SNAKEDIR}snakelogs/repro/
+
+# cd into directory containing snakefiles
+cd ${SNAKEDIR}snakefiles/
+
+# submit snakemake job
+# see: https://snakemake.readthedocs.io/en/stable/executable.html
+snakemake -j 100 --nolock --rerun-incomplete --cluster-config ../cluster_jsons/repro.json --cluster "sbatch -A compbio -p {cluster.partition} -n {cluster.nodes} -t {cluster.time} -J {cluster.J} -o {cluster.o} -e {cluster.e} --mem {cluster.mem}" -s Snakefile_repro
+
---
+++ snakemake/submitters/submit_slurm_repro.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+# To initiate a parallelized reproduction of this study using the slurm scheduler,
+# run the command "source submit_slurm_repro.sh" 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SNAKEDIR="$(readlink -f $DIR/../)/"
+SUBMITDIR=${SNAKEDIR}submitters/
+LOGDIR=${SNAKEDIR}snakelogs/repro/
+
+echo LOGDIR is $LOGDIR
+
+mkdir -p $LOGDIR
+
+cd ${SUBMITDIR}
+sbatch --account=compbio --time=3-01:00:00 --job-name=subber_ASD_mutex_repro --output=${LOGDIR}submitter_repro_%j.out --error=${LOGDIR}submitter_repro_%j.err --partition=long_jobs slurm_repro.sh
